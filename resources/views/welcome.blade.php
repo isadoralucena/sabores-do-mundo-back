8df9da0dc5c0fc41eae948248ddfa9e55df63f8b
ganestgamer11 <ganestgamer11@gmail.com>
2023-11-29 09:03:16 -0300

    home update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,11 +53,17 @@
         <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
         <h1 class="align-middle"><a href="index.html">Sabores do Mundo</a></h1>
     </div>
+    <div>
+      <form action="" id="search">
+        <input type="text" name="" id="" style="width: 200px">
+        <button>Enviar</button>
+      </form>
+      
+    </div>
     <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
         <li><a class="nav-link scrollto" href="index.html">Página inicial</a></li>
         <li><a class="nav-link scrollto" href="index.html #map">Continentes</a></li>
-        <li><a class="nav-link scrollto" href="index.html #forms">Formulário</a></li>
         <li><a class="nav-link scrollto" href="{{route('login')}}">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
@@ -113,6 +119,67 @@
 
   <main id="main">
 
+    {{-- Recent add section --}}
+    <section id="chefs" class="chefs">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Adicionados recentemente</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="recipe">
+              <div class="pic">
+                <img src="assets/img/generic.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="recipe-info">
+                <h4>Purê de batata</h4>
+                <span>Dona Maria</span>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="recipe">
+              <div class="pic">
+                <img src="assets/img/generic.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="recipe-info">
+                <h4>Purê de batata</h4>
+                <span>Dona Maria</span>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="recipe">
+              <div class="pic">
+                <img src="assets/img/generic.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="recipe-info">
+                <h4>Purê de batata</h4>
+                <span>Dona Maria</span>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <button type="submit" class="book-a-table-btn mx-0 border-0 mt-4">Ver mais</button>
+          </div>
+
+
+        </div>
+      </div>
+    </section><!-- end recent add section -->
+
     <!-- ======= Map Section ======= -->
     <section id="map">
       <div class="container">
@@ -124,215 +191,6 @@
       <div id="map">
       </div>
     </section>
-
-    <section id="forms">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Avalie-nos</h2>
-          <p>Valorizamos sua opinião! Sua avaliação construtiva é fundamental para aprimoramos nosso site</p>
-        </div>
-
-        <div class="row">
-          <form method="POST" action="forms/save.php">
-            <div class="form-row mx-auto col-10 col-md-8 col-lg-6 p-4 shadow bg-white">
-
-              <div class="form-group">
-                <p>Qual a sua idade?</p>
-                <select class="form-select" name="age">
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18 em diante">18 em diante</option>
-                </select>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>Você acha que o objetivo do site, no geral, ficou claro?</p>
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="clear_objective" id="inlineRadio1" value="Sim" checked>
-                    <label class="form-check-label" for="inlineRadio1">Sim</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="clear_objective" id="inlineRadio2" value="Não">
-                    <label class="form-check-label" for="inlineRadio2">Não</label>
-                  </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>Ao navegar pelo sistema pela primeira vez deu para entender que o objetivo é qual?</p>
-                <textarea required name="understanding_objective" class="form-control" placeholder="Campo obrigatório"></textarea>
-              </div>
-              <div class="form-group pt-5">
-                <p>As comidas típicas dos 5 continentes foi bem apresentada? Queremos saber se ficou claro que existe uma grande diversidade culinária de um país para outro.</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="culinary_diversity" id="inlineRadio1" value="Sim" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="culinary_diversity" id="inlineRadio2" value="Não">
-                  <label class="form-check-label" for="inlineRadio2">Não</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>A linguagem utilizada foi de fácil compreensão?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="understandable_language" id="inlineRadio1" value="Sim" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="understandable_language" id="inlineRadio2" value="Não">
-                  <label class="form-check-label" for="inlineRadio2">Não</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>O site tem que tipo de proposta?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="proposal" id="inlineRadio1" value="Informativo" checked>
-                  <label class="form-check-label" for="inlineRadio1">Informativo</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="proposal" id="inlineRadio2" value="Expositivo">
-                  <label class="form-check-label" for="inlineRadio2">Expositivo</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="proposal" id="inlineRadio2" value="Blog">
-                  <label class="form-check-label" for="inlineRadio2">Blog</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="proposal" id="inlineRadio2" value="E-Commerce">
-                  <label class="form-check-label" for="inlineRadio2">E-Commerce</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>A identidade visual (cores, tipo de letra, imagens) é agradável e harmoniosa?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="pleasant_visual_identity" id="inlineRadio1" value="Sim" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="pleasant_visual_identity" id="inlineRadio2" value="Não">
-                  <label class="form-check-label" for="inlineRadio2">Não</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>A identidade visual (cores, tipo de letra, imagens) remete ao tema da culinária?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="culinary_visual_identity" id="inlineRadio1" value="Sim" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="culinary_visual_identity" id="inlineRadio2" value="Não">
-                  <label class="form-check-label" for="inlineRadio2">Não</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>Existe um padrão de cores nas páginas?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="color_pattern" id="inlineRadio1" value="Sim, amarelo, preto e branco" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim, amarelo, preto e branco</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="color_pattern" id="inlineRadio2" value="Não, as cores são muito diversificadas">
-                  <label class="form-check-label" for="inlineRadio2">Não, as cores são muito diversificadas</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>A logomarca remete ao tema/objetivo do site?</p>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="coherent_logo" id="inlineRadio1" value="Sim" checked>
-                  <label class="form-check-label" for="inlineRadio1">Sim</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="coherent_logo" id="inlineRadio2" value="Não">
-                  <label class="form-check-label" for="inlineRadio2">Não</label>
-                </div>
-              </div>
-
-              <div class="form-group pt-5">
-                <p>Você sentiu falta de alguma funcionalidade, ou tem alguma sugestão para melhorar o site?</p>
-                <textarea placeholder="Campo opcional" name="suggestion" class="form-control"></textarea>
-              </div>
-
-             <div class="text-center">
-                <button type="submit" class="book-a-table-btn mx-0 border-0 mt-4">Avaliar</button>
-              </div>
-
-            </div>
-          </form>
-        </div>
-      </div>
-    </section>
-
-    <!-- ======= Chefs Section ======= -->
-    <section id="chefs" class="chefs">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Nossa equipe</h2>
-          <p>Nossa missão é despertar seu apetite em descobrir os sabores do mundo</p>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="assets/img/chefs/chefs-1.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Eric Rudnick</h4>
-                <span>Programador</span>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="assets/img/chefs/chefs-2.png" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Isadora Lucena</h4>
-                <span>Programadora</span>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="assets/img/chefs/chefs-3.jpg" class="img-fluid" alt=""></div>
-              <div class="member-info">
-                <h4>Maria Eduarda</h4>
-                <span>Programadora</span>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Chefs Section -->
 
   </main><!-- End #main -->
 
